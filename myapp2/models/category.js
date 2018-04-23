d96ae285e24a28d27536ef25961e4b7fd08472e7
231716573 <231716573@qq.com>
2018-04-24 00:05:24 +0800

    express教程

--- myapp2/models/category.js
+++ myapp2/models/category.js
@@ -1,1 +1,54 @@
 var Db = require('./db');
+var mongodb = new Db();
+
+function Category(order, name, alias, url) {
+	this.order = order;
+	this.name = name;
+	this.alias = alias;
+	this.url = url;
+}
+
+module.exports = Category;
+
+// 存储一个分类
+Category.prototype.save = function(callback) {
+	var date = new Date();
+	//存储各种时间格式，方便以后扩展
+  var time = {
+    date: Math.round(new Date() / 1000),
+    year : date.getFullYear(),
+    month : date.getFullYear() + "-" + (date.getMonth() + 1),
+    day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+    minute : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) 
+  }
+  // 要存入数据库的东西
+  var cate = {
+  	order: this.order,
+  	name: this.name,
+  	alias: this.alias,
+  	url: this.url,
+  	create_time: time
+  };
+
+  mongodb.insert(cate, 'category', function(err, result) {
+  	if (err) {
+  		return callback(err);
+  	}
+  	callback(null, cate)
+  })
+  
+}
+
+Category.get = function(name, callback) {
+  var query = {};
+  if (name) {
+    query.name = name
+  }
+
+  mongodb.find(query, 'category', function(err, result) {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, result)
+  })
+} 
--- myapp2/routes/category.js
+++ myapp2/routes/category.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var category = require('../models/category.js')
+
+router.get('/getCategory', function (req, res, next) {
+	category.get('', function (err, cate) {
+		res.send({
+			code: 0,
+			data: cate,
+			msg: '查询成功'
+		})
+	})
+})
 
 router.post('/createCategory', function(req, res, next) {
 	var order = req.body.order,
@@ -7,14 +18,36 @@ router.post('/createCategory', function(req, res, next) {
 			alias = req.body.alias,
 			url = req.body.url;
 
-	var data = {
-		order, name, alias, url
-	}
+	var newCategory = new category(order, name, alias, url)
 
-	res.send({
-		code: 0,
-		data: data
+
+	category.get(name, function (err, cate) {
+		if(cate[0]) {
+			res.send({
+				code: 2,
+				cate: cate[0],
+				msg: '此分类已存在！'
+			})
+			return 
+		}
+
+		// 如果不存在则新增分类
+		newCategory.save(function(err, user) {
+			if (err) {
+				res.send({
+					code: 2,
+	  			msg: '系统出错！'
+				})
+				return
+			}
+
+			res.send({
+				code: 0,
+				msg: '添加成功！'
+			})
+		})
 	})
+
 })
 
 module.exports = router;
